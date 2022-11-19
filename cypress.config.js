e909e2607e40af0f556d93b8004368b1163d9c43
svshy <dominik.such.96@gmail.com>
2022-11-19 15:27:31 +0100

    Add base url and video on false

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://api.github.com",
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
